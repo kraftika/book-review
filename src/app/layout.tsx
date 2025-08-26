9c85426f742cec521f19796a7178b48f289b21a6
Ionut Pistol <ionut.pistol.dev@gmail.com>
2025-08-26 23:24:47 +0300

    Fixed the build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Literata, Work_Sans } from "next/font/google";
+import { Literata } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const literata = Literata({
   subsets: ["latin-ext"],
   variable: "--font-literata",
-  display: "swap",
-});
-
-const workSans = Work_Sans({
-  subsets: ["latin-ext"],
-  variable: "--font-worksans",
   display: "swap",
 });
 
